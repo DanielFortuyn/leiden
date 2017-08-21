92f09f27d35b6589fce35307a779e13ad9a86fd2
Daniel Fortuyn <daniel@fortuyn.nl>
2017-08-21 15:56:41 +0200

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,10 @@ var twig = require('gulp-twig');
 var browserSync = require('browser-sync').create();
 var watch = require('gulp-watch');
 
-gulp.task('stream', function () {
-    // Endless stream mode
-    return watch('./src/*.twig')
-        .pipe(gulp.dest('data'));
-});
-
 // Global object to store returned data.
 var entries = null;
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "./www"
@@ -25,33 +19,37 @@ gulp.task('browser-sync', function() {
 });
 
 
-gulp.task('data', function(cb){
-  var client;
+gulp.task('data', function (cb) {
+    var client;
+    try {
+        client = contentful.createClient({
+            accessToken: '2e4752ec5edc713207f3367b623a8a2149606b385f0d5d68e1a35cf6ff54334c',
+            space: 'ljlrqlrwqoub'
+        });
+        return client.getEntries({'content_type': 'page', include: 4}).then(function (entry) {
+            return gulp.src('./src/index.twig')
+                .pipe(twig({
+                    data: {
+                        title: 'test',
+                        page: entry
+                    }
+                }))
+                .pipe(gulp.dest('./www/'));
+        });
+    } catch (e) {
+        console.error('Could not create Contenful client.', e);
+    }
+    return true;
+});
 
-  try {
-    client = contentful.createClient({
-      accessToken: '2e4752ec5edc713207f3367b623a8a2149606b385f0d5d68e1a35cf6ff54334c',
-      space: 'ljlrqlrwqoub'
-    });
-  } catch(e) {
-    console.error('Could not create Contenful client.', e);
-  }
-
-  client.getEntries({ 'content_type': 'page', include: 4}).then(function(entry) {
-      gutil.log(entry)
-      gulp.src('./src/index.twig')
-        .pipe(twig({
-            data: {
-              title: 'test',
-              page:  entry
-            }
-        }))
-        .pipe(gulp.dest('./www/'));
-  });
+gulp.task('default', ['watch'], function () {
 
 });
 
-gulp.task('default', ['data'], function(){
-  gutil.log(entries)
-  gutil.log('Retrieved contenful entries.');
+
+gulp.task('watch', function () {
+    var watcher = gulp.watch('src/*.twig', ['data']);
+    watcher.on('change', function (event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
 });
